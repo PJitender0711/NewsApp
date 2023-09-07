658392d4a386dff420d36d80f50c0c28c6e4c7f9
P Jitender <jituseervi321@gmail.com>
2023-09-07 08:34:11 +0530

    Adding Previous & Next Buttond to populate NewsItems

--- src/components/News.js
+++ src/components/News.js
@@ -3,43 +3,56 @@ import PropTypes from 'prop-types'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
-  articles = [
-    {
-      "source": {
-        "id": "espn-cric-info",
-        "name": "ESPN Cric Info"
-      },
-      "author": null,
-      "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-      "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-      "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-      "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-      "publishedAt": "2020-04-27T11:41:47Z",
-      "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-    },
-    {
-      "source": {
-        "id": "espn-cric-info",
-        "name": "ESPN Cric Info"
-      },
-      "author": null,
-      "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-      "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-      "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-      "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-      "publishedAt": "2020-03-30T15:26:05Z",
-      "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-    }
-  ]
+
   constructor() {
     super();
     console.log("Hello I am a constructor from News Component");
     this.state = {
-      articles: this.articles,
-      loading: false
+      articles: [],
+      loading: false,
+      page: 1
     }
   }
+  // ek async function apni body k andar wait kr saktaa he kuch promisses ke resolve hone ka
+  async componentDidMount() {
+    console.log("cdm");
+    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=a6be903c18604fb29611931379e92eeb&page=1&pageSize=20"
+    //to ye func intezaar krega "let data = await fetch(url);" is promise k poora hone ka
+    let data = await fetch(url);
+    let parsedData = await data.json()
+    console.log(parsedData);
+    this.setState({ articles: parsedData.articles, totalArticles: parsedData.totalResults })
+  }
 
+  handlePreClick = async () => {
+    console.log("Previous clicked!");
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=a6be903c18604fb29611931379e92eeb&page=${this.state.page - 1}&pageSize=20`
+    //to ye func intezaar krega "let data = await fetch(url);" is promise k poora hone ka
+    let data = await fetch(url);
+    let parsedData = await data.json()
+    console.log(parsedData);
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles
+    })
+  }
+  handleNextClick = async () => {
+    if (this.state.page + 1 > Math.ceil(this.state.totalArticles / 20)) {
+      alert("this is the last page");
+    }
+    else {
+      console.log("Next clicked!");
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=a6be903c18604fb29611931379e92eeb&page=${this.state.page + 1}&pageSize=20`
+      //to ye func intezaar krega "let data = await fetch(url);" is promise k poora hone ka
+      let data = await fetch(url);
+      let parsedData = await data.json()
+      console.log(parsedData);
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsedData.articles
+      })
+    }
+  }
   render() {
     return (
       <div className='container my-3'>
@@ -48,14 +61,18 @@ export class News extends Component {
         <div className="row">
           {this.state.articles.map((element) => {
             return <div className="col-md-4" key={element.url}>
-              <NewsItem  title={element.title.slice(0,40)} description={element.description.slice(0,80)} imgUrl={element.urlToImage} newsUrl={element.url} />
+              <NewsItem title={element.title ? element.title.slice(0, 40) : ""} description={element.description ? element.description.slice(0, 80) : ""} imgUrl={element.urlToImage ? element.urlToImage : "https://images.wsj.net/im-841784/social"} newsUrl={element.url} />
             </div>
           })}
 
+        </div>
+        <div className='container d-flex justify-content-between'>
+          <button disabled={this.state.page <= 1} type="button" className="btn btn-dark " onClick={this.handlePreClick}> &larr; previous</button>
+          <button type="button" className="btn btn-dark " onClick={this.handleNextClick}>Next &rarr; </button>
         </div>
       </div>
     )
   }
 }
 
-export default News
+export default News;
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -10,7 +10,7 @@ export class NavBar extends Component {
         return (
             <div>
 
-                <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                <nav className="navbar navbar-expand-lg  bg-body-tertiary">
                     <div className="container-fluid">
                         <a className="navbar-brand" href="/">JSNews</a>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
